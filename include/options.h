9d6761547d89f22a26a72b9778ddd12864f04f55
actinium <andreas.cederholm@gmail.com>
2016-01-27 04:33:31 +0100

    Add options struct.

--- include/options.h
+++ include/options.h
@@ -1,8 +1,15 @@
 #ifndef SEARCH_OPTIONS_H
 #define SEARCH_OPTIONS_H
 
-class options{
-
+struct program_options{
+  bool show_help;
+  bool show_version;
+  bool include_hidden_files;
+  bool files_only;
+  bool directories_only;
+  int  number_of_result_lines;
 };
+
+program_options get_options(int argc, char* argv[]);
 
 #endif
