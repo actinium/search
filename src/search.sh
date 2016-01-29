3ebe2dda86625c0bd79026798a089bfd013cb272
actinium <andreas.cederholm@gmail.com>
2016-01-29 02:02:51 +0100

    Add creation of pipe in search script.

--- src/search.sh
+++ src/search.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-./search_searcher $@
+tmppipe=$(mktemp -u "/tmp/search.pipe.XXXXXX")
+
+if mkfifo -m 600 "$tmppipe"; then
+  trap 'rm -rf "$tmppipe"' EXIT INT TERM HUP
+  ./search_searcher $@
+else
+  echo "Error: Could not create fifo."
+fi
 
 exit 0
