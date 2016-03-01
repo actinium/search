e50cf83e8a5d4bd661d4c1f231873c17cc8c6b84
actinium <andreas.cederholm@gmail.com>
2016-03-01 14:49:24 +0100

    Add Methods to query_string.

--- include/query_string.h
+++ include/query_string.h
@@ -5,12 +5,26 @@
 
 class query_string{
  public:
-  query_string(const char* s):str(s){}
+  query_string():str(""),pos(0){}
+  query_string(const char* s):str(s),pos(0){}
   const char* get_str(){
     return str.c_str();
   }
+  void add(char c){
+    // add c at pos
+  }
+  void remove(){
+    // remove char before pos
+  }
+  void cursor_left(){
+    // pos left
+  }
+  void cursor_right(){
+    // pos right
+  }
  private:
   std::string str;
+  std::size_t pos;
 };
 
 #endif
