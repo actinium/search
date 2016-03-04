5055020fd717a743b5b7ef12c72e1a9480012c2a
actinium <andreas.cederholm@gmail.com>
2016-03-04 08:53:41 +0100

    Make filefinder ignore hidden files and directories.

--- src/filefinder.cpp
+++ src/filefinder.cpp
@@ -1,4 +1,5 @@
 #include "filefinder.h"
+#include "options.h"
 
 #if ! defined(_XOPEN_SOURCE) || _XOPEN_SOURCE < 600
 #define _XOPEN_SOURCE 600       /* Get nftw() and S_IFSOCK declarations */
@@ -12,10 +13,19 @@ std::vector<node> *nodes;
 int dirTree(const char *pathname, const struct stat *, int type,
         struct FTW *ftwb){
   std::string filename(&pathname[ftwb->base]);
-  std::string location(pathname);
-  location = location.substr(0,location.size()-filename.size());
-  nodes->emplace_back(filename,location,ftwb->level, type == FTW_D);
-  return 0;
+  if(filename == "."){
+    return 0;
+  }
+  if(filename[0] != '.' || options.include_hidden){
+    std::string location(pathname);
+    location = location.substr(0,location.size()-filename.size());
+    nodes->emplace_back(filename,location,ftwb->level, type == FTW_D);
+    return 0;
+  }else if(type != FTW_D){
+    return 0;
+  }else{
+    return FTW_SKIP_SUBTREE;
+  }
 }
 
 }
