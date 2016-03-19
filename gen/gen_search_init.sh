ac8a320f456782bb2784a35377ee5984512a45a3
actinium <andreas.cederholm@gmail.com>
2016-03-19 02:15:27 +0100

    Add check for emtpty return string. Don't change directory after -h or -v.

--- gen/gen_search_init.sh
+++ gen/gen_search_init.sh
@@ -2,10 +2,12 @@
 
 SEARCHDIR=${1-$(pwd)};
 
-echo "function search {" > $SEARCHDIR/init_search;
+echo "function search {"                     >  $SEARCHDIR/init_search;
 echo "  OUTPUT=\$($SEARCHDIR/searcher \$@);" >> $SEARCHDIR/init_search;
-echo "  cd \${OUTPUT};" >> $SEARCHDIR/init_search;
-echo "}" >> $SEARCHDIR/init_search;
+echo "  if [ -n \"\$OUTPUT\" ]; then"        >> $SEARCHDIR/init_search;
+echo "    cd \${OUTPUT};"                    >> $SEARCHDIR/init_search;
+echo "  fi"                                  >> $SEARCHDIR/init_search;
+echo "}"                                     >> $SEARCHDIR/init_search;
 
 
 exit 0;
