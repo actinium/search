9ef04987f157a40cf388cde9a6dd803bbb494b3b
actinium <andreas.cederholm@gmail.com>
2016-04-05 21:20:18 +0200

    Remove directory prefix from init_search when installed.

--- gen/gen_search_init.sh
+++ gen/gen_search_init.sh
@@ -2,12 +2,17 @@
 
 SEARCHDIR=${1-$(pwd)};
 
-echo "function search {"                     >  $SEARCHDIR/init_search;
-echo "  OUTPUT=\$($SEARCHDIR/searcher \$@);" >> $SEARCHDIR/init_search;
-echo "  if [ -n \"\$OUTPUT\" ]; then"        >> $SEARCHDIR/init_search;
-echo "    cd \${OUTPUT};"                    >> $SEARCHDIR/init_search;
-echo "  fi"                                  >> $SEARCHDIR/init_search;
-echo "}"                                     >> $SEARCHDIR/init_search;
+echo "function search {"                       >  $SEARCHDIR/init_search;
+# Don't prefix searcher with pwd when installing to /usr/local/bin
+if [ "$SEARCHDIR" = "/usr/local/bin" ]; then
+  echo "  OUTPUT=\$(searcher \$@);"            >> $SEARCHDIR/init_search;
+else
+  echo "  OUTPUT=\$($SEARCHDIR/searcher \$@);" >> $SEARCHDIR/init_search;
+fi
+echo "  if [ -n \"\$OUTPUT\" ]; then"          >> $SEARCHDIR/init_search;
+echo "    cd \${OUTPUT};"                      >> $SEARCHDIR/init_search;
+echo "  fi"                                    >> $SEARCHDIR/init_search;
+echo "}"                                       >> $SEARCHDIR/init_search;
 
 
 exit 0;
